d345659f16ec25a9cc6af325e7063813e5a5c960
Adamstbright <113826509+Adamstbright@users.noreply.github.com>
2023-05-31 22:04:10 +0100

    customize admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    """
+    This class is basically to add more features to the admin. i.e to perform
+    other function different from default admin model.
+    """
+    list_display = ("first_name", "last_name", "email")
+    search_field = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    ordering = ("-first_name", )
+    readonly_fields = ("occupation",)
+
+
+admin.site.register(Form, FormAdmin)
